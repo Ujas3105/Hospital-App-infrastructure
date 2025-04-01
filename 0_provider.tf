b765bd7cc7cda36d0c43bec7bad2d56994ebbd5b
ujas <ujasayodeji@gmail.com>
2025-04-01 14:29:47 +0100

    destroy

--- 0_provider.tf
+++ 0_provider.tf
@@ -16,10 +16,10 @@ provider "azurerm" {
   subscription_id = var.subscription_id  
 }
 
-variable "client_id" {}  #7fb8c580-799d-496d-9d53-72a81451cb30
-variable "client_secret" {} #0304b316-167c-4389-94d6-cdf28407d1a4
-variable "tenant_id" {} #89756a7e-858c-4132-85e8-16cbddc4eb55
-variable "subscription_id" {} #b817b533-8f44-41cd-bf4e-1ea318323387
+variable "client_id" {}  
+variable "client_secret" {} 
+variable "tenant_id" {} 
+variable "subscription_id" {}
 
 # terraform-azure-pipeline  is the name of the ( App Registration )
 # permission for the App Registration 
